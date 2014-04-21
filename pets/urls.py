f9d421eaa0bffe564d95e4a913194fb951b70d0f
Andres Vega <andrezvega@hotmail.com>
2014-04-21 17:29:15 -0500

    Registro y login

--- pets/urls.py
+++ pets/urls.py
@@ -7,4 +7,9 @@ urlpatterns = patterns('',
     url(r'^$','principal.views.index'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^registro/$','principal.views.registro'),
+    url(r'^login/$','principal.views.login'),
+    url(r'^nuestros/medicos/$','principal.views.nuestrosMedicos'),
+    url(r'^preguntas/frecuentes/$','principal.views.preguntasFrecuentes'),
+
 )
--- principal/views.py
+++ principal/views.py
@@ -1,6 +1,63 @@
+#encoding:utf-8
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.http import HttpResponse, HttpResponseRedirect
+
+""" PAQUETES USUARIOS  """
+
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required                       
+
+# Aquí va todo lo que esta en el index todo el contenido de la página inicial 
 
 def index(request):
-	
-	return render_to_response('index.html',context_instance=RequestContext(request))
+	formulario = UserCreationForm()
+        formularioLogin = AuthenticationForm()
+	return render_to_response('index.html',{'formulario':formulario, 'formularioLogin':formularioLogin},context_instance=RequestContext(request))
+
+
+def nuestrosMedicos(request):
+    formulario = UserCreationForm()
+    formularioLogin = AuthenticationForm()
+    return render_to_response('nuestros_medicos.html',{'formulario':formulario,'formularioLogin':formularioLogin},context_instance=RequestContext(request))
+
+def preguntasFrecuentes(request):
+    formulario = UserCreationForm()
+    formularioLogin = AuthenticationForm()
+    return render_to_response('preguntas_frecuentes.html',{'formulario':formulario,'formularioLogin':formularioLogin},context_instance=RequestContext(request))
+
+# registro 
+
+def registro(request):
+    if request.method=='POST':
+        formulario = UserCreationForm(request.POST)
+        if formulario.is_valid:
+            formulario.save()
+            return HttpResponseRedirect('/')
+    else:
+        formulario = UserCreationForm()
+        return HttpResponseRedirect('/')
+
+#________________________Fin registro    
+
+
+def login(request):
+    if not request.user.is_anonymous():
+        return HttpResponseRedirect('/bienvenido')
+    if request.method == 'POST':
+        formulario = AuthenticationForm(request.POST)
+        if formulario.is_valid:
+            usuario = request.POST['username']
+            clave = request.POST['password']
+            acceso = authenticate(username=usuario, password=clave)
+            if acceso is not None:
+                if acceso.is_active:
+                    login(request, acceso)
+                    return render_to_response('index.html', context_instance=RequestContext(request))
+                else:
+                    return render_to_response('noactivo.html', context_instance=RequestContext(request))
+            else:
+                return render_to_response('index.html', context_instance=RequestContext(request))
+      
